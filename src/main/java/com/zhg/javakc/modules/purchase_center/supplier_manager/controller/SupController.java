5d03bc76b09827f457ebd29799f2e181d2a459be
35338 <liu15530493708@163.com>
2019-11-23 16:50:11 +0800

    供应商展示

--- src/main/java/com/zhg/javakc/modules/purchase_center/supplier_manager/controller/SupController.java
+++ src/main/java/com/zhg/javakc/modules/purchase_center/supplier_manager/controller/SupController.java
@@ -1,7 +1,25 @@
 package com.zhg.javakc.modules.purchase_center.supplier_manager.controller;
 
+import com.zhg.javakc.base.page.Page;
+import com.zhg.javakc.modules.purchase_center.supplier_manager.entity.SupplierEntity;
+import com.zhg.javakc.modules.purchase_center.supplier_manager.service.SupService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
+@RequestMapping("/sup")
 public class SupController {
+    @Autowired
+    private SupService supService;
+    @RequestMapping("/query")
+    public String querySup(SupplierEntity supplierEntity, HttpServletRequest request, HttpServletResponse response, ModelMap model){
+        model.put("page",supService.findSup(new Page(request,response),supplierEntity));
+        model.put("entity",supplierEntity);
+        return "/purchase_center/supplier_manager/list";
+    }
 }
--- src/main/java/com/zhg/javakc/modules/purchase_center/supplier_manager/entity/SupplierEntity.java
+++ src/main/java/com/zhg/javakc/modules/purchase_center/supplier_manager/entity/SupplierEntity.java
@@ -3,4 +3,105 @@ package com.zhg.javakc.modules.purchase_center.supplier_manager.entity;
 import com.zhg.javakc.base.entity.BaseEntity;
 
 public class SupplierEntity extends BaseEntity<SupplierEntity> {
+    // ## 主键
+    private String supId;
+    // ## 供应商名称
+    private String supName;
+    // ## 描述
+    private String supDescripte;
+    // ## 详细地址
+    private String supAddress;
+    // ## 联系人
+    private String infoContacts;
+    // ## 联系方式
+    private String  infoPhone;
+    // ## 备用联系人
+    private String infoContacts2;
+    // ## 备用联系人的联系方式
+    private String info_phone2;
+    // ## 地区
+    private String infoRcjin;
+    // ## 备注
+    private String infoRemarkes;
+
+
+    public String getSupId() {
+        return supId;
+    }
+
+    public void setSupId(String supId) {
+        this.supId = supId;
+    }
+
+    public String getSupName() {
+        return supName;
+    }
+
+    public void setSupName(String supName) {
+        this.supName = supName;
+    }
+
+    public String getSupDescripte() {
+        return supDescripte;
+    }
+
+    public void setSupDescripte(String supDescripte) {
+        this.supDescripte = supDescripte;
+    }
+
+    public String getSupAddress() {
+        return supAddress;
+    }
+
+    public void setSupAddress(String supAddress) {
+        this.supAddress = supAddress;
+    }
+
+    public String getInfoContacts() {
+        return infoContacts;
+    }
+
+    public void setInfoContacts(String infoContacts) {
+        this.infoContacts = infoContacts;
+    }
+
+    public String getInfoPhone() {
+        return infoPhone;
+    }
+
+    public void setInfoPhone(String infoPhone) {
+        this.infoPhone = infoPhone;
+    }
+
+    public String getInfoContacts2() {
+        return infoContacts2;
+    }
+
+    public void setInfoContacts2(String infoContacts2) {
+        this.infoContacts2 = infoContacts2;
+    }
+
+    public String getInfo_phone2() {
+        return info_phone2;
+    }
+
+    public void setInfo_phone2(String info_phone2) {
+        this.info_phone2 = info_phone2;
+    }
+
+    public String getInfoRcjin() {
+        return infoRcjin;
+    }
+
+    public void setInfoRcjin(String infoRcjin) {
+        this.infoRcjin = infoRcjin;
+    }
+
+    public String getInfoRemarkes() {
+        return infoRemarkes;
+    }
+
+    public void setInfoRemarkes(String infoRemarkes) {
+        this.infoRemarkes = infoRemarkes;
+    }
 }
