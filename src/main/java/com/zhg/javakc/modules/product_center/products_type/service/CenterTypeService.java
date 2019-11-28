a63e16486a5e4b567b74a78562d616a8075b1500
wcy <wcy13014391428@163.com>
2019-11-28 11:46:55 +0800

    基础类目

---
+++ src/main/java/com/zhg/javakc/modules/product_center/products_type/service/CenterTypeService.java
@@ -0,0 +1,22 @@
+package com.zhg.javakc.modules.product_center.products_type.service;
+
+import com.zhg.javakc.base.page.Page;
+import com.zhg.javakc.base.service.BaseService;
+import com.zhg.javakc.modules.product_center.products_type.dao.CenterTypeDao;
+import com.zhg.javakc.modules.product_center.products_type.entity.CenterTypeEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class CenterTypeService extends BaseService<CenterTypeDao, CenterTypeEntity> {
+    @Autowired
+    CenterTypeDao typeDao;
+    public List<Map<String,Object>> queryType(){
+
+        return typeDao.queryType();
+    }
+
+}
